15577a0dd190e86bf0d80eab5681fa8f120d50b1
Sebastian Kasanzew <sebkasanzew@gmail.com>
2015-11-08 21:01:23 +0100

    created test scene with autogenerated mesh

---
+++ Assets/Pottery/Scripts/createMeshFromVertices.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createMeshFromVertices : MonoBehaviour {
+
+    public float radius;
+    public GameObject inputSpline;
+
+    private Vector3[] newVertices;
+    private Vector2[] newUV;
+    private int[] newTriangles;
+
+    void Start() {
+
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+
+        newVertices = new []
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(0, 0, 1),
+            new Vector3(0, 1, 0),
+            new Vector3(0, 1, 1)
+        };
+
+        newUV = new []
+        {
+            new Vector2(0, 0),
+            new Vector2(0, 0),
+            new Vector2(0, 1),
+            new Vector2(0, 1)
+        };
+
+        newTriangles = new[]
+        {
+            2, 1, 0,
+            1, 2, 3
+        };
+
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.vertices = newVertices;
+        mesh.uv = newUV;
+        mesh.triangles = newTriangles;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
