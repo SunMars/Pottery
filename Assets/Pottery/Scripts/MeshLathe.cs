b6df979cd96660fb0601d9f7bd02ac9a449e082e
Sebastian Kasanzew <sebkasanzew@gmail.com>
2015-11-08 23:03:05 +0100

    started implementing Lathe from spline

---
+++ Assets/Pottery/Scripts/MeshLathe.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshLathe {
+
+    private Vector3 spline;
+    private GameObject go;
+    private int sections;
+    private float radius;
+
+    /// <summary>
+    /// Main contructor.
+    /// </summary>
+    /// <param name="spline">A spline as an array of <c>Vector3</c> elements</param>
+    /// <param name="go">The empty <c>GameObject</c> on which the lathe mesh will be drawn</param>
+    /// <param name="radius">default is <c>0</c></param>
+    MeshLathe(Vector3 spline, GameObject go, int sections, float radius = 0)
+    {
+        this.spline = spline;
+        this.go = go;
+        this.sections = sections;
+        this.radius = radius;
+
+        
+    }
+
+    private Vector3[] createPolygon (Vector3[] splineSection)
+    {
+        if (splineSection.Length != 2)
+        {
+            Debug.Log("createPolygon(): invalid length of spline section Vector");
+        }
+
+
+
+        return new Vector3[] { };
+    }
+
+    private void createTriangle(Vector3 a, Vector3 b, Vector3 c)
+    {
+
+    }
+
+    // TODO rename
+    private float getUmfang(float radius)
+    {
+
+        return 9f;
+    }
+}
--- Assets/Pottery/Scripts/createMeshFromVertices.cs
+++ Assets/Pottery/Scripts/createMeshFromVertices.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class createMeshFromVertices : MonoBehaviour {
 
     public float radius;
-    public GameObject inputSpline;
+    public Material mat;
+
+    private Vector3[] spline;
 
     private Vector3[] newVertices;
     private Vector2[] newUV;
@@ -14,6 +16,17 @@ public class createMeshFromVertices : MonoBehaviour {
 
         gameObject.AddComponent<MeshFilter>();
         gameObject.AddComponent<MeshRenderer>();
+
+        Renderer renderer = gameObject.GetComponent<MeshRenderer>();
+
+        spline = new []
+        {
+            new Vector3(0, 0, 1),
+            new Vector3(0, 1, 2),
+            new Vector3(0, 2, 2),
+            new Vector3(0, 3, 2),
+            new Vector3(0, 4, 1),
+        };
 
         newVertices = new []
         {
@@ -42,10 +55,30 @@ public class createMeshFromVertices : MonoBehaviour {
         mesh.vertices = newVertices;
         mesh.uv = newUV;
         mesh.triangles = newTriangles;
+
+        renderer.material = mat;
     }
 
     // Update is called once per frame
     void Update () {
 	
 	}
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="spline"></param>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    Vector3[] createMeshVertices (Vector3[] spline)
+    {
+        Vector3[] meshVertices = new Vector3[spline.Length - 1];
+
+        foreach(Vector3 vert in spline)
+        {
+            //meshVertices.
+        }
+
+        return new Vector3[] { };
+    }
 }
