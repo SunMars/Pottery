79951907d71654dc0f48f5e9fa899727aa7230aa
Sebastian Kasanzew <sebkasanzew@gmail.com>
2015-11-08 17:18:08 +0100

    fixed .gitignore added all *.meta files

---
+++ Assets/Pottery/Scripts/getLeapHandPosition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class _getLeapHandPosition : MonoBehaviour {
+
+    public GameObject[] rightHand;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
