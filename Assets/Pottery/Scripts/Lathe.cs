e3d8f85aec20db305fec5447543fc3690f1cae66
Sebastian Kasanzew <s58457@beuth-hochschule.de>
2015-11-12 00:27:14 +0100

    updated Lathe class now with working vertices generation

--- Assets/Pottery/Scripts/Lathe.cs
+++ Assets/Pottery/Scripts/Lathe.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 
 /// <summary>
-/// This script created a lathed object from a curve/spline.
+/// This script creates a lathed object from a curve/spline.
 /// </summary>
 [ExecuteInEditMode]
 public class Lathe : MonoBehaviour
@@ -27,17 +27,17 @@ public class Lathe : MonoBehaviour
         // ---------------------------
 
         // public vars ---------------
-
+        public List<Vector3> spline;
         // ---------------------------
 
         // protected vars ------------
-        protected List<Vector3> spline;
         protected GameObject gameObject;
         protected int sections;
         protected Material mat;
 
         // private vars --------------
         // needed for mesh creating
+        private Mesh mesh;
         private List<Vector3> vertices;
         private List<Vector2> uv;
         private int[] triangles;
@@ -45,7 +45,7 @@ public class Lathe : MonoBehaviour
         //////////////////////////////
 
         /// <summary>
-        /// Main contructor.
+        /// Main constructor.
         /// </summary>
         /// <param name="spline">A spline as an array of <c>Vector3</c> elements</param>
         /// <param name="gameObject">The empty <c>GameObject</c> on which the lathe mesh will be drawn</param>
@@ -56,27 +56,22 @@ public class Lathe : MonoBehaviour
             this.sections = sections;
             this.mat = mat;
 
-            // make sure there are minimum 3 sections
-            if (this.sections > 3)
-            {
-                this.sections = 3;
-            }
+            List<List<Vector3>> verticesList2D = getLathedMeshVertices(spline, sections);
 
-            Renderer renderer = gameObject.GetComponent<MeshRenderer>();
+            this.mesh = mesh;
+            this.vertices = list2dToSimpleList(verticesList2D);
+            //this.uv = TODO;
+            this.triangles = getTriangleArray(verticesList2D);
 
-            mesh.vertices = vertices.ToArray();
-            mesh.uv = uv.ToArray();
-            mesh.triangles = triangles;
-
-            renderer.material = mat;
+            createMesh();
         }
 
         /// <summary>
-        /// Standard contructor. (Only for testing)
+        /// Standard constructor. (Only for testing)
         /// </summary>
         public LathedObject(GameObject gameObject, int sections, Material mat, Mesh mesh, MeshFilter meshFilter)
         {
-            spline = new List<Vector3>
+            this.spline = new List<Vector3>
             {
                 new Vector3(0, 0, 1),
                 new Vector3(0, 1, 2),
@@ -88,7 +83,7 @@ public class Lathe : MonoBehaviour
             // new LathedObject(spline, gameObject, sections, mat, mesh, meshFilter);
 
             // -------------------------
-            vertices = new List<Vector3>
+            this.vertices = new List<Vector3>
             {
                 new Vector3(0, 0, 0),
                 new Vector3(0, 0, 1),
@@ -96,7 +91,7 @@ public class Lathe : MonoBehaviour
                 new Vector3(0, 1, 1)
             };
 
-            uv = new List<Vector2>
+            this.uv = new List<Vector2>
             {
                 new Vector2(0, 0),
                 new Vector2(0, 0),
@@ -104,7 +99,7 @@ public class Lathe : MonoBehaviour
                 new Vector2(0, 1)
             };
 
-            triangles = new int[]
+            this.triangles = new int[]
             {
                 2, 1, 0,
                 1, 2, 3
@@ -118,6 +113,42 @@ public class Lathe : MonoBehaviour
             renderer.material = mat;
         }
 
+        // TODO
+        private void createMesh()
+        {
+            mesh.vertices = vertices.ToArray();
+            //mesh.uv = uv.ToArray();
+            mesh.triangles = triangles;
+
+            Renderer renderer = gameObject.GetComponent<MeshRenderer>();
+            renderer.material = mat;
+        }
+
+        /// <summary>
+        /// This is where the magic happens. The function calculates the triangles for the lathed mesh from the generated vertices.
+        /// </summary>
+        /// <param name="verticesList2D">A 2D list of the vertices of the mesh.</param>
+        /// <returns>An array of the vertex indizes of the triangles.</returns>
+        private int[] getTriangleArray(List<List<Vector3>> verticesList2D)
+        {
+            // number of triangles = horizontal sections * vertical sections * triangles per polygon
+            int[] triangleArray = new int[sections * verticesList2D.Count * 2];
+
+            for (int i = 0; i < spline.Count; i++)
+            {
+                Vector3 firstVertex = verticesList2D[i][0];
+                // TODO add all triangles to the array
+            }
+
+            // TODO only for testing
+            triangleArray = new int[] {
+                0, 1, 1 + sections,
+                1 + sections, 0 + sections, 0
+            };
+
+            return triangleArray;
+        }
+
         /// <summary>
         /// Get a computed list of all vertices for a given number of segments on a circle with (0, 0, 0) as origin.
         /// </summary>
@@ -126,11 +157,11 @@ public class Lathe : MonoBehaviour
         /// <returns>A list of all vertices on a circle in the same height as the given vertex.</returns>
         private List<Vector3> getVerticesOnCircle(Vector3 vertex, int segments)
         {
-            float radius = Vector3.Distance(vertex, Vector3.up);
-            float segmentAngle = getAngleForNumberOfSections(segments);
             Vector3 circleOrigin = new Vector3(0, vertex.y, 0);
+            float radius = Vector3.Distance(vertex, circleOrigin);
+            float segmentAngle = getAngleForNumberOfSections(segments);
 
-            List<Vector3> verticesOnCircle = new List<Vector3> { vertex };
+            List<Vector3> verticesOnCircle = new List<Vector3> { };
 
             for (float angle = 0f; angle < 2 * Mathf.PI; angle += segmentAngle)
             {
@@ -144,22 +175,20 @@ public class Lathe : MonoBehaviour
         /// Get all a 2D-list of all vertices for the generated lathed mesh. 
         /// </summary>
         /// <param name="spline">The spline from which the lathed object will be created.</param>
-        /// <param name="segments">The number of segments the mesh will be created with.</param>
+        /// <param name="sections">The number of sections the mesh will be created with.</param>
         /// <returns>A 2D-list of all mesh vertices.
         /// e.g.: <code>
-        /// [
-        /// [0, 1, 0],
-        /// [0, 2, 1],
-        /// [0, 3, 4], 
-        /// [0, 4, 3]
-        /// ]</code></returns>
-        private List<List<Vector3>> getLathedMeshVertices(List<Vector3> spline, int segments)
+        /// [ [0, 1, 0],
+        ///   [0, 2, 1],
+        ///   [0, 3, 4], 
+        ///   [0, 4, 3] ]</code></returns>
+        private List<List<Vector3>> getLathedMeshVertices(List<Vector3> spline, int sections)
         {
             List<List<Vector3>> meshVertices = new List<List<Vector3>>();
 
             foreach (Vector3 splineVertex in spline)
             {
-                meshVertices.Add(getVerticesOnCircle(splineVertex, segments));
+                meshVertices.Add(getVerticesOnCircle(splineVertex, sections));
             }
 
             return meshVertices;
@@ -170,18 +199,17 @@ public class Lathe : MonoBehaviour
         /// </summary>
         /// <param name="verticesList2d"></param>
         /// <returns>A one dimension <c>List&ltVector3&gt</c></returns>
-        private List<Vector3> List2dToSimpleList(List<List<Vector3>> verticesList2d)
+        private List<Vector3> list2dToSimpleList(List<List<Vector3>> verticesList2d)
         {
             List<Vector3> verticesList = new List<Vector3>() { };
 
-            foreach(List<Vector3> verticesOnCircle in verticesList2d)
+            foreach (List<Vector3> verticesOnCircle in verticesList2d)
             {
                 verticesList.AddRange(verticesOnCircle);
             }
 
             return verticesList;
         }
-        
 
         /// <summary>
         /// Get the angle of a segment in radians.
@@ -193,6 +221,7 @@ public class Lathe : MonoBehaviour
             return 2 * Mathf.PI / segments;
         }
 
+        // TODO not finished
         private void createPolygon(int[] splineSection, List<int> triangles)
         {
             if (splineSection.Length != 2)
@@ -245,7 +274,7 @@ public class Lathe : MonoBehaviour
             // default axis is the up vector
             if (rotationAxis == null)
             {
-                rotationAxis = Vector3.up;    
+                rotationAxis = Vector3.up;
             }
 
             return Vector3.Distance(vertex, rotationAxis.Value);
@@ -274,12 +303,21 @@ public class Lathe : MonoBehaviour
             // x = cx + r * cos(a)
             // y = cy               | same height as the origin of the circle
             // z = cz + r * sin(a)
-            return new Vector3(origin.x + (radius * Mathf.Cos(angle)), origin.y, origin.z + (radius * Mathf.Sin(angle)));
+            return new Vector3(origin.x + (radius * Mathf.Sin(angle)), origin.y, origin.z + (radius * Mathf.Cos(angle)));
         }
 
         internal void draw()
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generates a string with the configured number of sections and the generated vertices. 
+        /// </summary>
+        /// <returns>A <c>String</c> with the number of sections and generated vertices.</returns>
+        public override String ToString()
+        {
+            return "Lathed Object --------\nSections: " + sections + " | Vertices: " + vertices.ToArray().Length;
         }
     }
 
@@ -288,12 +326,22 @@ public class Lathe : MonoBehaviour
         MeshFilter meshFilter = GetComponent<MeshFilter>();
         mesh = new Mesh();
         meshFilter.mesh = mesh;
-        lathedObject = new LathedObject(gameObject, sections, material, mesh, meshFilter);
+
+        List<Vector3> spline = new List<Vector3>
+        {
+            new Vector3(0, 0, 1),
+            new Vector3(0, 1, 2),
+            new Vector3(0, 2, 2),
+            new Vector3(0, 3, 2),
+            new Vector3(0, 4, 1),
+        };
+
+        lathedObject = new LathedObject(spline, gameObject, sections, material, mesh, meshFilter);
     }
 
     void Update()
     {
         // redraw the mesh every frame
-        // test.draw();
+        // lathedObject.draw();
     }
 }
